47a19b8807a389f35721e252dd2c2b1e8b61bc11
Eliude de Lima Felizindo <eliudeangelo@gmail.com>
2019-05-31 00:35:08 +0000

    Trabalho2.html

--- Trabalho2.js
+++ Trabalho2.js
@@ -1,15 +1,14 @@
-//observei na pagina https://turismosaovicente.com.br/ tem uma classe chamada """line lft""" que esta em uma div e depois vem outra div e depois vem os """as"""
-//mais parecido com a pagina da fatec
-//porem não funcionou, acredito que tem mais alguma coisa que temos que mudar
-
 function montarPagina(pagina){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
-    documento.querySelectorAll(".banner_roteiros lft").forEach(function(div){
-        var ul = div.children;
+    documento.querySelectorAll(".rs-fm-js").forEach(function(main){
+        var ul = main.children;
         Array.from(ul).forEach(function(li){
             var noticia = li.children;
-            Array.from(noticia).forEach(x => document.body.appendChild(x));
+            Array.from(noticia).forEach(function(x){
+                
+            document.body.appendChild(x);
+            });
         })
         //var div2 = document.createElement("div");
         //div2.innerHTML = p;
@@ -23,11 +22,11 @@ function sendReq() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         montarPagina(this.responseText);
-        alert("Oi!");
+       
       }
     };
-  
-  xhttp.open("GET", "https://burles.co/", true);
+  //servidor do Arthur: https://cors-server-arthurhmk.c9users.io/
+  xhttp.open("GET", " https://www.msn.com/pt-br/entretenimento/famosos", true);
   xhttp.send();
 }
 
@@ -43,4 +42,12 @@ function criarEvento(){
 
 window.onload = criarEvento;
 
-// https://burles.co/ - Funciona
+// https://gente.ig.com.br/ - Funciona
+// https://www.msn.com/pt-br/entretenimento/famosos - Funciona até sem servidor, precisamos de mais um
+
+//http://rduirapuru.com.br/noticias/
+//https://www.sonoticias.com.br/
+//https://www.nsctotal.com.br/dc
+//https://alfenashoje.com.br/
+//herald-mob-nav
+
